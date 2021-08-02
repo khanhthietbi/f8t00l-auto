7937a95e26e705eb5df14de8487b273ba6aedcc1
khanhthietbi <khanhthietbi@gmail.com>
2021-08-02 10:20:49 +0700

    Update 2.target.js

--- 2.target.js
+++ 2.target.js
@@ -1,5 +1,2 @@
 https://www.facebook.com/American-Veterans-106231094986776
-https://www.facebook.com/just5moremins
-https://www.facebook.com/KimLongMonTea
 https://www.facebook.com/kanemadeit
-https://www.facebook.com/HanaremyBJD
